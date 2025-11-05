f9a9a139d373bea5379ba813286267e1750d0b0a
Sparsh <codewithsparshjain@gmail.com>
2025-11-05 13:08:26 +0530

    tuples in typescript

---
+++ 09_tuples.ts
@@ -0,0 +1,6 @@
+type User = [string,number];
+// by defining this that means anyone who will use User he can only insert string at [0] and number at [1]
+
+const user1:User = ["Sparsh",1];
+// const user2: User = [1,"Sparsh"] // not allowed 
+
